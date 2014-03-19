161e53eb6de44375220dcbce016138e62a693f1c
SladeThe <sladethe@gmail.com>
2014-03-19 15:26:21 +0400

    - improved UrlUtilTest.

--- code/src/test/java/com/codeforces/commons/text/UrlUtilTest.java
+++ code/src/test/java/com/codeforces/commons/text/UrlUtilTest.java
@@ -29,4 +29,32 @@ public class UrlUtilTest extends TestCase {
                 )
         );
     }
+
+    public void testReplaceParameterInUrl() throws Exception {
+        assertEquals(
+                "http://localhost/?pageIndex=999",
+                UrlUtil.replaceParameterInUrl("http://localhost/?", "pageIndex", "999")
+        );
+        assertEquals(
+                "http://localhost/?pageIndex=pageIndex",
+                UrlUtil.replaceParameterInUrl("http://localhost/?pageIndex=0", "pageIndex", "pageIndex")
+        );
+        assertEquals(
+                "http://localhost/?pageIndex=350#pageIndex",
+                UrlUtil.replaceParameterInUrl("http://localhost/?pageIndex=1#pageIndex", "pageIndex", "350")
+        );
+        assertEquals("http://127.0.0.1?id#id", UrlUtil.replaceParameterInUrl("http://127.0.0.1#id", "id", null));
+        assertEquals(
+                "http://google.ru/?pageIndex&id=17&pageIndex1&counter=0&flag=1",
+                UrlUtil.replaceParameterInUrl(
+                        "http://google.ru/?id=17&pageIndex=0&pageIndex1&counter=0&pageIndex&flag=1", "pageIndex", null
+                )
+        );
+        assertEquals(
+                "http://google.ru/?pageIndex1=0#?id=17&pageIndex=0&pageIndex1&counter=0&pageIndex&flag=1",
+                UrlUtil.replaceParameterInUrl(
+                        "http://google.ru/#?id=17&pageIndex=0&pageIndex1&counter=0&pageIndex&flag=1", "pageIndex1", "0"
+                )
+        );
+    }
 }
