b1040525ad9a8ec2b92712819bc6486ea0aabd9e
fefer.ivan <fefer.ivan@gmail.com>
2014-06-04 17:39:54 +0400

    added mime type application/javascript to support jsonp callbacks in codeforces api

--- code/src/main/java/com/codeforces/commons/io/MimeUtil.java
+++ code/src/main/java/com/codeforces/commons/io/MimeUtil.java
@@ -92,6 +92,7 @@ public class MimeUtil {
         public static final String TEXT_CSS = "text/css";
         public static final String APPLICATION_XML = "application/xml";
         public static final String APPLICATION_JSON = "application/json";
+        public static final String APPLICATION_JAVASCRIPT = "application/javascript";
 
         public static final String APPLICATION_PDF = "application/pdf";
         public static final String APPLICATION_X_TEX = "application/x-tex";
