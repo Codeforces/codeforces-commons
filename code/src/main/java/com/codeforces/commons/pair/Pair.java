a4d5bdf19abbc7c9c6cb5c0f4978230cd875d86f
SladeThe <sladethe@gmail.com>
2016-07-28 13:06:21 +0300

    Moved some code from Pair to SimplePair.

--- code/src/main/java/com/codeforces/commons/pair/Pair.java
+++ code/src/main/java/com/codeforces/commons/pair/Pair.java
@@ -9,6 +9,7 @@ import javax.annotation.Nullable;
  * @author Maxim Shipko (sladethe@gmail.com)
  *         Date: 11.07.13
  */
+@SuppressWarnings("ComparableImplementedButEqualsNotOverridden")
 public class Pair<F extends Comparable<? super F>, S extends Comparable<? super S>>
         extends SimplePair<F, S> implements Comparable<Pair<F, S>> {
     public Pair() {
@@ -57,37 +58,6 @@ public class Pair<F extends Comparable<? super F>, S extends Comparable<? super 
 
         return 0;
     }
-
-    public boolean equals(@Nullable F first, @Nullable S second) {
-        return (this.first == null ? first == null : this.first.equals(first))
-                && (this.second == null ? second == null : this.second.equals(second));
-    }
-
-    @SuppressWarnings("NonFinalFieldReferenceInEquals")
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-
-        if (!(o instanceof Pair)) {
-            return false;
-        }
-
-        Pair pair = (Pair) o;
-
-        return (first == null ? pair.first == null : first.equals(pair.first))
-                && (second == null ? pair.second == null : second.equals(pair.second));
-    }
-
-    @SuppressWarnings("NonFinalFieldReferencedInHashCode")
-    @Override
-    public int hashCode() {
-        int result = first == null ? 0 : first.hashCode();
-        result = 32323 * result + (second == null ? 0 : second.hashCode());
-        return result;
-    }
-
 
     @Override
     public String toString() {
--- code/src/main/java/com/codeforces/commons/pair/SimplePair.java
+++ code/src/main/java/com/codeforces/commons/pair/SimplePair.java
@@ -1,5 +1,7 @@
 package com.codeforces.commons.pair;
 
+import com.codeforces.commons.text.StringUtil;
+
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
@@ -44,5 +46,40 @@ public class SimplePair<F, S> {
 
     public void setSecond(@Nullable S second) {
         this.second = second;
+    }
+
+    public boolean equals(@Nullable F first, @Nullable S second) {
+        return (this.first == null ? first == null : this.first.equals(first))
+                && (this.second == null ? second == null : this.second.equals(second));
+    }
+
+    @SuppressWarnings("NonFinalFieldReferenceInEquals")
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (!(o instanceof SimplePair)) {
+            return false;
+        }
+
+        SimplePair pair = (SimplePair) o;
+
+        return (first == null ? pair.first == null : first.equals(pair.first))
+                && (second == null ? pair.second == null : second.equals(pair.second));
+    }
+
+    @SuppressWarnings("NonFinalFieldReferencedInHashCode")
+    @Override
+    public int hashCode() {
+        int result = first == null ? 0 : first.hashCode();
+        result = 32323 * result + (second == null ? 0 : second.hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return StringUtil.toString(this, false, "first", "second");
     }
 }
