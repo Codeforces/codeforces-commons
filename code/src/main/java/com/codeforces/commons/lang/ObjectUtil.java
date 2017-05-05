77efcb227b56c3252cdbc3eb1152c82835045c98
SladeThe <sladethe@gmail.com>
2017-05-05 15:11:52 +0400

    Added many methods to ObjectUtil.

--- code/src/main/java/com/codeforces/commons/lang/ObjectUtil.java
+++ code/src/main/java/com/codeforces/commons/lang/ObjectUtil.java
@@ -13,6 +13,7 @@ import java.util.function.Supplier;
  * @author Maxim Shipko (sladethe@gmail.com)
  *         Date: 10.03.2017
  */
+@SuppressWarnings("WeakerAccess")
 public class ObjectUtil {
     private ObjectUtil() {
         throw new UnsupportedOperationException();
@@ -20,14 +21,66 @@ public class ObjectUtil {
 
     @Contract("null, null -> fail")
     @Nonnull
-    public static <T> T toNotNull(@Nullable T value, @Nonnull T nullReplacement) {
+    public static <T> T toNotNull(@Nullable T value, @Nullable T nullReplacement) {
         return value == null ? Preconditions.checkNotNull(nullReplacement) : value;
+    }
+
+    @Contract("null, null, null -> fail")
+    @Nonnull
+    public static <T> T toNotNull(@Nullable T value, @Nullable T nullReplacementA, @Nullable T nullReplacementB) {
+        return value == null ? toNotNull(nullReplacementA, nullReplacementB) : value;
     }
 
     @Contract("null, null -> fail")
     @Nonnull
     public static <T> T toNotNull(@Nullable T value, @Nonnull Supplier<T> nullReplacement) {
         return value == null ? Preconditions.checkNotNull(nullReplacement.get()) : value;
+    }
+
+    @Contract("null, null, _ -> fail")
+    @Nonnull
+    public static <T> T toNotNull(
+            @Nullable T value, @Nonnull Supplier<T> nullReplacementA, @Nonnull Supplier<T> nullReplacementB) {
+        return value == null ? toNotNull(nullReplacementA.get(), nullReplacementB) : value;
+    }
+
+    @Contract("null, null, _ -> fail")
+    @Nonnull
+    public static <T> T toNotNull(
+            @Nullable T value, @Nonnull Supplier<T> nullReplacementA, @Nullable T nullReplacementB) {
+        return value == null ? toNotNull(nullReplacementA.get(), nullReplacementB) : value;
+    }
+
+    @Contract("null, null, null -> fail")
+    @Nonnull
+    public static <T> T toNotNull(
+            @Nullable T value, @Nullable T nullReplacementA, @Nonnull Supplier<T> nullReplacementB) {
+        return value == null ? toNotNull(nullReplacementA, nullReplacementB) : value;
+    }
+
+    @Contract("null, null -> null; !null, _ -> !null; _, !null -> !null")
+    @Nullable
+    public static <T> T defaultIfNull(@Nullable T value, @Nullable T nullReplacement) {
+        return value == null ? nullReplacement : value;
+    }
+
+    @Contract("null, null, null -> null; !null, _, _ -> !null; _, !null, _ -> !null; _, _ , !null-> !null")
+    @Nullable
+    public static <T> T defaultIfNull(@Nullable T value, @Nullable T nullReplacementA, @Nullable T nullReplacementB) {
+        return value == null ? defaultIfNull(nullReplacementA, nullReplacementB) : value;
+    }
+
+    @Contract("null, null -> fail; !null, _ -> !null")
+    @Nullable
+    public static <T> T computeIfNull(@Nullable T value, @Nonnull Supplier<T> nullReplacement) {
+        return value == null ? nullReplacement.get() : value;
+    }
+
+    @Contract("null, null, _ -> fail; !null, _, _ -> !null")
+    @Nullable
+    public static <T> T computeIfNull(
+            @Nullable T value, @Nonnull Supplier<T> nullReplacementA, @Nonnull Supplier<T> nullReplacementB) {
+        return value == null ? computeIfNull(nullReplacementA.get(), nullReplacementB) : value;
     }
 
     @Contract("!null, null -> fail")
