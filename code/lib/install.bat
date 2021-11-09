6cb5d51a1c8517a7ffd670b2483a3bf5dd80cc2a
MikeMirzayanov <mirzayanovmr@gmail.com>
2021-11-09 14:41:53 +0300

    Use system deps instead of install.bat

--- code/lib/install.bat
+++ code/lib/install.bat
@@ -1,2 +1,2 @@
-mvn install:install-file -Dfile="daisydiff-1.2-NX5-SNAPSHOT.jar" -DgroupId=org.outerj.daisy -DartifactId=daisydiff -Dversion=1.2-NX5-SNAPSHOT -Dpackaging=jar
-mvn install:install-file -Dfile="guice-5.0.2-SNAPSHOT.jar" -DgroupId=com.google.inject -DartifactId=guice -Dversion=5.0.2-SNAPSHOT -Dpackaging=jar
+call mvn install:install-file -Dfile="daisydiff-1.2-NX5-SNAPSHOT.jar" -DgroupId=org.outerj.daisy -DartifactId=daisydiff -Dversion=1.2-NX5-SNAPSHOT -Dpackaging=jar
+call mvn install:install-file -Dfile="guice-5.0.2-SNAPSHOT.jar" -DgroupId=com.google.inject -DartifactId=guice -Dversion=5.0.2-SNAPSHOT -Dpackaging=jar
